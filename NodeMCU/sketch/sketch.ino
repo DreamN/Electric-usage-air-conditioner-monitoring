4477d16891d8ddd10e53de19ec8cd27ee48f7086
Isara Naranirattisai <isaradream@gmail.com>
2017-05-23 04:16:27 +0700

    feat: Get html from server

--- NodeMCU/sketch/sketch.ino
+++ NodeMCU/sketch/sketch.ino
@@ -1,9 +1,24 @@
 #include <ESP8266WiFi.h>
+#define SERVER_PORT 80
+
 const char* ssid     = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
+
+const char* server_ip = "192.168.1.244";
+
+String get_path = "GET /";
+String param = " HTTP/1.1\r\n";
+String server_host = "Host: 192.168.1.244\r\n\r\n";
+
+WiFiServer server(SERVER_PORT);
+WiFiClient client;
+
+unsigned long previousMillis = 0;    
+const long interval = 10000;
+
 void setup() 
 {
-    Serial.begin(115200);
+    Serial.begin(115200); 
     delay(10);
     Serial.println();
     Serial.println();
@@ -23,10 +38,41 @@ void setup()
     WiFi.config(local_ip,gateway,subnet);
  
     Serial.println(""); 
-    Serial.println("WiFi connected");
+    Serial.println("WiFi connected"); 
     Serial.println("IP address: "); 
     Serial.println(WiFi.localIP());
 }
 void loop() {
-}
 
+    // print data from server
+    while(client.available())        
+    {
+          String line = client.readStringUntil('\n');
+          Serial.println(line);
+    }
+    
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
+        Client_Request();
+    }  
+}
+void Client_Request()
+{
+    Serial.println("Connect TCP Server");
+    int cnt=0;
+    while (!client.connect(server_ip,SERVER_PORT))
+    {
+          Serial.print(".");
+          delay(100);
+          cnt++;
+          if(cnt>50)
+          return;
+    } 
+    Serial.println("Success");
+    delay(500);
+    client.print(get_path+param+server_host);
+    Serial.print(get_path+param+server_host);
+    delay(100);
+}
