f30d15e9f64eeabeb59e9651f7a30b8c01764d2d
Isara Naranirattisai <isaradream@gmail.com>
2017-05-24 01:22:43 +0700

    fix: Datetime to minutes

--- WebServer/models.py
+++ WebServer/models.py
@@ -13,13 +13,18 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+def dt_to_minutes(dt):
+    days, hours, minutes = t.days, t.seconds // 3600, t.seconds // 60 % 60
+    return (days*24*60)+(hours*60)+minutes
+
 class Device(Base):
     __tablename__ = 'device'
     id = Column(String(20), primary_key=True)
     status = Column(Boolean, default=False)
     aircon = Column(Boolean, default=False)
     totalTime = Column(BigInteger, default = 0)
-    last_update = Column(DateTime, default = datetime.datetime.utcnow)
+    last_update = Column(DateTime, default = datetime.datetime.now())
 
     def __init__(self, id):
         print('Registed device id : ' + id)
@@ -29,9 +34,9 @@ class Device(Base):
         self.status = status
         self.aircon = aircon
         if(self.status == False):
-            timediff = datetime.datetime.utcnow - self.last_update
+            timediff = dt_to_minutes(datetime.datetime.now() - self.last_update)
             self.totalTime += timediff
-        self.last_update = datetime.datetime.utcnow
+        self.last_update = datetime.datetime.now()
 
 
 Base.metadata.create_all(engine)
