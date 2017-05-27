ab93bdda2bc7cdf49df1e8e807c065efd62e316d
Isara Naranirattisai <isaradream@gmail.com>
2017-05-28 02:03:46 +0700

    feat: DHCP for nodemcu

--- NodeMCU/sketch/sketch.ino
+++ NodeMCU/sketch/sketch.ino
@@ -38,22 +38,17 @@ void setup()
     Serial.print("Connecting to ");
     Serial.println(ssid);
     WiFi.begin(ssid, password);
-   
+    
     while (WiFi.status() != WL_CONNECTED)
     {
             delay(500);
             Serial.print(".");
     }
-     
-    IPAddress local_ip = {192,168,1,144};
-    IPAddress gateway={192,168,1,1};
-    IPAddress subnet={255,255,255,0};
-    WiFi.config(local_ip,gateway,subnet);
- 
     Serial.println(""); 
-    Serial.println("WiFi connected"); 
-    Serial.println("IP address: "); 
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");   
     Serial.println(WiFi.localIP());
+    
 }
 void loop() {
 
